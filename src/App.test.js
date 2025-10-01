e6b74c1d4ecbd69f31cfe1215e545d1a67c7f342
YugantarDev <yugi19892008@gmail.com>
2025-10-01 13:19:40 +0530

    New Files of react

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders site logo', () => {
-  render(<App />)
-  // Get the logo link by accessible name
-  const logoElement = screen.getByRole('link', { name: /Nakshaveda/i })
-  expect(logoElement).toBeInTheDocument()
-})
